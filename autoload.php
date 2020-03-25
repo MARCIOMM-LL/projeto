15d964b9a0e123101a2b633958d651706bf3f989
Márcio <32685719+MARCIOMM-LL@users.noreply.github.com>
2020-03-25 16:54:55 -0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -4,6 +4,7 @@ spl_autoload_register(
     function (string $namespaceClasse): void {
         $caminho = "/src";
         $diretorio_classe = str_replace("\\", DIRECTORY_SEPARATOR, $namespaceClasse);
+        #O arroba é pra suorimir o erro em caso de o arquivo possuir erros ou não existir
         @include_once getcwd() . $caminho . DIRECTORY_SEPARATOR . "{$diretorio_classe}.php";
     } 
 );
